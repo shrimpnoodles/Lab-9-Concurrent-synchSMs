ffb36eaab503d97a50212c8c5f29c704ca6aeea6
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-02-14 09:36:16 -0800

    Update hcier001_lab9_part2.c

--- turnin/hcier001_lab9_part2.c
+++ turnin/hcier001_lab9_part2.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	submission link: https://drive.google.com/file/d/1Gu8JJY3N-VzPK1QG4fpCv-uDPYHHe8IR/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
